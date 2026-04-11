bd3eed75837ffa4639aa1d90f0adfb74f74f6db5
Matt Alvarez <65744803+alvamatt045@users.noreply.github.com>
2026-04-10 20:12:56 -0500

    Update views.py
    put all auth views in views py

--- poke_shop/shop/views.py
+++ poke_shop/shop/views.py
@@ -1,3 +1,114 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from poke_shop.shop.models import Customer
+
+from django.http import HttpResponse
+
+## home page
+def home(request):
+    return render(request, 'home.html')
+
+'''
+AuthenticationController Views. 
+'''
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Customer.objects.create(user=user)  # Create associated Customer profile
+            login(request, user)
+        return redirect('shop-home')
+    else:
+        form = UserCreationForm()
+
+
+    return render(request, 'shop/register.html', {'form': form})
+
+def login_view(request):
+    next_url = request.GET.get('next', 'shop-home')  # Default to home if no next parameter
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if user.is_staff:
+                return redirect('shop-dashboard')  # Redirect admins to dashboard
+            return redirect('shop-home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'shop/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('shop-home')
+'''
+Catalog Views
+'''
+def catalog(request):
+    return render(request, 'catalog.html')
+
+'''
+ListingController Views
+'''
+## individual lisitng
+def listing_detail(request):
+    return render(request, 'listing.html')
+
+
+'''
+CartController Views
+'''
+## cart page
+def cart(request):
+    return render(request, 'cart/cart.html')
+def add_to_cart(request):
+    return render(request, 'cart/cart_add.html')
+def remove_from_cart(request):
+    return render(request, 'remove_from_cart.html')
+def update_cart_quantity(request):
+    return render(request, 'update_cart_quantity.html')
+
+'''
+Orders Views
+'''
+## checkout page
+def order_history(request):
+    return render(request, 'orders/order_history.html')
+def order_detail(request):
+    return render(request, 'orders/order_detail.html')
+## checkout page
+def cancel_order(request):
+    return render(request, 'orders/cancel_order.html')
+
+'''
+Checkout Views
+'''
+## checkout page
+def checkout(request):
+    return render(request, 'checkout/checkout.html')
+def order_confirmation(request):
+    return render(request, 'checkout/order_confirmation.html')
+
+'''
+Admin Dashboard Views
+'''
+def admin_dashboard(request):
+    return render(request, 'admin/admin_dashboard.html')
+def create_listing(request):
+    return render(request, 'admin/create_listing.html')
+def edit_listing(request):
+    return render(request, 'admin/edit_listing.html')
+def remove_listing(request):
+    return render(request, 'admin/remove_listing.html')
+def all_orders(request):
+    return render(request, 'admin/all_orders.html')
+def update_order_status(request):
+    return render(request, 'admin/update_order_status.html')
+def update_tracking(request):
+    return render(request, 'admin/update_tracking.html')
+
