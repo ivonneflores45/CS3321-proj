d25649a3a95eee01e4ffe085c15a98b53a7e7d18
Matt Alvarez <65744803+alvamatt045@users.noreply.github.com>
2026-04-20 15:27:47 -0500

    Checkout Views, portions of admin

--- poke_project/shop/views.py
+++ poke_project/shop/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Listing
+from .models import Listing, Order, OrderItems, Payment, ShippingInfo, Customer
 from .filters import ListingFilter
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -27,7 +27,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             Customer.objects.create(user=user)
-            login(request, user) #log em in immediately after registering
+            login(request, user) 
             return redirect('shop-home')    
     else:
         form = UserCreationForm()
@@ -146,6 +146,7 @@ def update_cart_quantity(request, id):
 Orders Views
 '''
 def order_history(request):
+
     return render(request, 'orders/order_history.html')
 def order_detail(request):
     return render(request, 'orders/order_detail.html')
@@ -154,23 +155,146 @@ def cancel_order(request):
 
 '''
 Checkout Views
+Matt Alvarez
 '''
 ## checkout page
 def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        return redirect('shop-cart')
+    if request.method == 'POST':
+    # Collect info
+        recipient_name = request.POST.get('recipient_name')
+        address = request.POST.get('address')  
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip_code')
+        country = request.POST.get('country')  
+        payment_method = request.POST.get('payment_method')
+
+        customer = Customer.objects.get(user=request.user)
+        total = customer.total_amount #function to calculate total from cart items
+        order = Order.objects.create(customer=customer, total_amount=total ) 
+
+
+        for listing_id, quantity in cart.items():
+            listing = get_object_or_404(Listing, id=listing_id)
+            OrderItems.objects.create(
+                order=order,
+                listing=listing,
+                quantity=quantity,
+                unit_price=listing.base_price
+            )
+        ShippingInfo.objects.create(
+            customer=Customer,
+            order=order,
+            recipient_name=recipient_name,
+            address_line1=address,
+            city=city,
+            state=state,
+            zipcode=zip_code,
+            country=country
+        )
+
+        Payment.objects.create(
+            order=order,
+            amount=total,
+            payment_method=payment_method,
+            payment_status='completed',
+        )
+        request.session['cart'] = {}
+        return redirect('shop-order-confirmation')
     
-    return render(request, 'checkout/checkout.html')
+    cart_items = []
+    total = 0
+    for listing_id, quantity in cart.items():
+        listing = get_object_or_404(Listing, id=listing_id)
+        subtotal = listing.base_price * quantity
+        total += subtotal
+        cart_items.append({
+            'listing':listing,
+            'quantity':quantity,
+            'subtotal':subtotal,
+        })
+    return render(request, 'checkout/checkout.html', {
+        'cart_items':cart_items,
+        'total':total,
+    })  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def order_confirmation(request):
-    return render(request, 'checkout/order_confirmation.html')
+
+    Customer.objects.get(user=request.user)
+    order = Order.objects.filter(customer=Customer).latest('order_date')
+    order_items = OrderItems.objects.filter(order=order)
+    shipping = ShippingInfo.objects.get(order=order)
+
+    return render(request, 'checkout/order_confirmation.html', {
+        'order': order,
+        'order_items': order_items,
+        'shipping': shipping
+    }   )
 
 '''
 Admin Dashboard Views
 '''
+def admin_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.is_staff:
+            return redirect('shop-login')
+        return view_func(request, *args, **kwargs)
+    return wrapper
+@admin_required
 def admin_dashboard(request):
-    return render(request, 'admin/admin_dashboard.html')
+    total_orders = Order.objects.count()
+    total_revenue = Payment.objects.filter(payment_status='completed').aggregate(total=models.Sum('amount'))['total'] or 0
+    total_listings = Listing.objects.count()
+    active_listings = Listing.objects.filter(listing_status='active').count()
+
+    return render(request, 'admin/admin_dashboard.html', {
+        'total_orders': total_orders,
+        'total_revenue': total_revenue,
+        'total_listings': total_listings,
+        'active_listings': active_listings
+    }   )
+@admin_required
 def create_listing(request):
+    if request.method == 'POST':
+        listing = Listing.objects.create(
+            title=request.POST.get('title'),
+            description=request.POST.get('description'),
+            base_price=request.POST.get('base_price'),
+            stock_quantity=request.POST.get('stock_quantity'),
+            category=request.POST.get('category'),
+            condition=request.POST.get('condition'),
+            rarity=request.POST.get('rarity'),
+            element=request.POST.get('element'),
+            type=request.POST.get('type'),
+            set_name=request.POST.get('set_name'),
+            image = request.FILES.get('image')
+        )
+
+        return redirect('shop-dashboard')
     return render(request, 'admin/create_listing.html')
+@admin_required
 def edit_listing(request):
     return render(request, 'admin/edit_listing.html')
+@admin_required
 def remove_listing(request):
     return render(request, 'admin/remove_listing.html')
 def all_orders(request):
